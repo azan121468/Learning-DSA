865614a219d670859a1b4b6b305fe6e63d3ea209
azan121468 <67824275+azan121468@users.noreply.github.com>
2025-12-27 00:35:01 +0500

    implement trie: refactor to reuse search logic for prefix checks

--- Implement-Trie-Prefix-Tree/solution.py
+++ Implement-Trie-Prefix-Tree/solution.py
@@ -1,55 +1,46 @@
-# for this problem i just modified the solution i wrote for add and search word
 import random
 
 class TrieNode:
-    def __init__(self, char='', isEnd=False):
-        self.char = char
+    def __init__(self, is_end=False):
         self.next = {}   # next character: node of next character
-        self.isEnd = isEnd
+        self.is_end = is_end  #PEP8
 
 class Trie:
-
     def __init__(self):
-        self.trie = TrieNode()
+        self.head = TrieNode()
 
     def insert(self, word: str) -> None:
-        head = self.trie
-        for char in word:
-            if char in head.next.keys():
-                head = head.next[char]
-            else:
-                head.next[char] = TrieNode(char)
-                head = head.next[char]
-        head.isEnd = True
+        cur = self.head
 
-    def search(self, word: str) -> bool:
-        head = self.trie
         for char in word:
-            if char in head.next.keys():
-                head = head.next[char]
-            else:
+            if char not in cur.next:
+                cur.next[char] = TrieNode()
+            cur = cur.next[char]
+
+        cur.is_end = True
+
+    def search(self, word: str, search_full_word: bool = True) -> bool:
+        #search_full_word argument is required to make prefix check possible without requiring additional function. 
+        cur = self.head
+
+        for char in word:
+            if char not in cur.next:
                 return False
-        return head.isEnd
+            cur = cur.next[char]
+        
+        return cur.is_end if search_full_word else True
 
     def startsWith(self, prefix: str) -> bool:
-        head = self.trie
-        for char in prefix:
-            if char not in head.next.keys():
-                return False
-            else:
-                head = head.next[char]
-        
-        return True
-
+        return self.search(prefix, search_full_word=False)
 
 c = Trie()
 
-to_insert = ['Azan', 'Shahid', 'Hacker', 'AsliWalaHacker', 'Hacker']
+to_insert = ['Azan', 'Shahid', 'Testing', '1234', 'ABCDEF']
 for val in to_insert:
     c.insert(val)
 
 assert c.search(random.choice(to_insert))
-assert not c.search('Wala')
+assert not c.search('Hacker')
 
 random_choice = random.choice(to_insert)
 random_prefix = random_choice[:random.choice(range(0, len(random_choice)-1))]
