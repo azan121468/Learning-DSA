895a4d5c7f4303ccb4b80b4ff92f0e257444cb62
azan121468 <67824275+azan121468@users.noreply.github.com>
2026-01-23 23:24:59 +0500

    added solution of graph valid tree

---
+++ Graph-Valid-Tree/solution.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from samples import *
+
+def validTree(n: int, edges: list[list[int]]) -> bool:
+    if n == 0:      #empty input is also considered valid tree
+        return True
+
+    #make adjacency list
+    adj = defaultdict(list)
+    for n1, n2 in edges:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
+    
+    visited = set()
+    def dfs(cur, parent):
+        if cur in visited:  #cycle is detected
+            return False
+        
+        visited.add(cur)
+
+        for child in adj[cur]:
+            #in undirected graph, visiting parent from chlid will lead to False cycle detection and parent and child both point to each other so we won't visit.
+            if child == parent:
+                continue
+
+            #if dfs return False due to cycle, immediately return False
+            if not dfs(child, cur):
+                return False
+        
+        return True
+
+    return dfs(0, -1) and len(visited) == n  #second condition is to make sure that we have visited all nodes and there are no orphan nodes.
+
+ans = validTree(*input1)
+print(ans)
+ans = validTree(*input2)
+print(ans)
