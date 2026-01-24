288b7796e8894fcc30788080d145b68b6ae342a9
azan121468 <67824275+azan121468@users.noreply.github.com>
2026-01-24 06:07:39 +0500

    added solution of rotate image

---
+++ Rotate-Image/solution.py
@@ -0,0 +1,35 @@
+from samples import *
+
+def rotate(matrix: list) -> None:
+    r, c = len(matrix), len(matrix[0]) 
+    diag_idx = 0
+
+    #main loop is to move along diagonal
+    while diag_idx < r:
+        #1. Swap element of row and column in current diagonal cell.
+        rr, cc = diag_idx, diag_idx  #rr: row-wise movment, cc: column-wise movement
+        while cc < c:
+            matrix[rr][diag_idx], matrix[diag_idx][cc] = matrix[diag_idx][cc], matrix[rr][diag_idx]
+            cc += 1
+            rr += 1
+
+        diag_idx += 1
+    
+    #2. now, we just have to reverse each row
+    for rn in range(r):
+        i, j = 0, c - 1
+        while i < j:
+            matrix[rn][i], matrix[rn][j] = matrix[rn][j], matrix[rn][i]
+            i += 1
+            j -= 1
+
+    #3. Return the result
+    return matrix
+
+
+ans = rotate(input1)
+print(ans)
+ans = rotate(input2)
+print(ans)
+ans = rotate(input3)
+print(ans)
